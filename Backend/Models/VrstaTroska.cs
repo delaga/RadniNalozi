c225935e5afddab78e2b784e90f8084073a977da
Karlo Bilić <karlo.bilic2003@gmail.com>
2025-03-07 19:25:23 +0100

    Popravio VrsteTroska

--- Backend/Models/VrstaTroska.cs
+++ Backend/Models/VrstaTroska.cs
@@ -1,7 +1,10 @@
 using System.ComponentModel.DataAnnotations.Schema;
 
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Backend.Models
 {
+    [Table("vrstaTroskova")]
     public class VrstaTroska : Entitet
     {
         public string Naziv { get; set; } = "";
